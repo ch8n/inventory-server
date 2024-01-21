1f1096634fdbc30e1fd72c97182d617e2bc59e35
ch8n <chetan.garg36@gmail.com>
2024-01-21 14:48:48 +0530

    wip carsoule offers clicked

--- frontend/src/lib/components/pages/the-fashion-jewellery.svelte
+++ frontend/src/lib/components/pages/the-fashion-jewellery.svelte
@@ -3,12 +3,15 @@
 
 	import CategoryGrid from '$lib/components/sections/category-grid.svelte';
 	import Title from '$lib/components/sections/title.svelte';
+
+	let offerUrls: string[] = [];
+	const onOfferClicked = (url: string) => {};
 </script>
 
 <div class="py-4">
 	<Title label={'The Fashion Jewellery'} />
 
-	<OfferCarousels />
+	<OfferCarousels {offerUrls} {onOfferClicked} />
 
 	<div class="py-4">
 		<div class="text-xl bold">Explore</div>
--- frontend/src/lib/components/sections/carousel-offers.svelte
+++ frontend/src/lib/components/sections/carousel-offers.svelte
@@ -1,26 +1,36 @@
-<script lang="ts">	
+<script lang="ts">
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
 	import Autoplay from 'embla-carousel-autoplay';
 	const plugin = Autoplay({ delay: 2000, stopOnInteraction: true });
+	export let offerUrls: string[] = [];
+	export const onOfferClicked = (offerUrl: string) => {};
 </script>
 
-<Carousel.Root
-	plugins={[plugin]}
-	class="w-full max-w-xs"
-	on:mousenter={plugin.stop}
-	on:mouseleave={plugin.reset}
->
-	<Carousel.Content>
-		{#each Array(5) as _, i (i)}
-			<Carousel.Item>
-				<div class="p-1">
-					<p>
-						<span class="text-4xl font-semibold">{i + 1}</span>
-					</p>
-				</div>
-			</Carousel.Item>
-		{/each}
-	</Carousel.Content>
-	<Carousel.Previous />
-	<Carousel.Next />
-</Carousel.Root>
+<div class="my-4">
+	{#if offerUrls.length != 0}
+		<Carousel.Root
+			plugins={[plugin]}
+			class="w-full max-w-xs"
+			on:mousenter={plugin.stop}
+			on:mouseleave={plugin.reset}
+		>
+			<Carousel.Content>
+				{#each offerUrls as url}
+					<Carousel.Item>
+						<div class="p-1 h-64 border border-gray-400 rounded-lg">
+							<button
+								on:click={(e) => {
+									onOfferClicked(url);
+								}}
+							>
+								<img class="h-full w-full object-cover" src={url} />
+							</button>
+						</div>
+					</Carousel.Item>
+				{/each}
+			</Carousel.Content>
+			<Carousel.Previous class="mx-8" />
+			<Carousel.Next class="mx-8" />
+		</Carousel.Root>
+	{/if}
+</div>
