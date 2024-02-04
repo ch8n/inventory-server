1cbeedd4640c415e0a072019561ad244252e0cf4
ch8n <chetan.garg36@gmail.com>
2024-02-04 17:57:33 +0530

    add to cart wip

--- frontend/src/routes/category/[id]/product/[id]/+page.svelte
+++ frontend/src/routes/category/[id]/product/[id]/+page.svelte
@@ -5,9 +5,11 @@
 	import type { OfferPagerItem, Product } from '$lib/data/HomePage';
 	import { onMount } from 'svelte';
 	import { cart, type CartItem } from '$lib/stores/cartStore';
-	import { goto } from '$app/navigation';
+	import { goto, } from '$app/navigation';
 	import { Button } from '$lib/components/ui/button';
-	import { ShoppingCart } from 'lucide-svelte';
+	import { ShoppingCart, ShoppingBag, ArrowBigLeft } from 'lucide-svelte';
+	import { browser } from "$app/environment";
+
 
 	let productId = $page.params.id;
 
@@ -17,7 +19,7 @@
 
 	$: {
 		if (product === null) {
-			onFetchProduct();
+			onFetchProduct(productId);
 		} else {
 			getPagerItems(product);
 			onFetchVarients(product);
@@ -35,7 +37,7 @@
 		});
 	};
 
-	const onFetchProduct = async () => {
+	const onFetchProduct = async (productId:string) => {
 		try {
 			const response = await fetch(`http://0.0.0.0:8080/v1/products?id=${productId}`);
 			if (response.ok) {
@@ -113,49 +115,88 @@
 	};
 </script>
 
-<div class="mx-auto px-6 py-6">
-	<OfferCarousels offerPagerItem={bannerImages} onOfferClicked={() => {}} />
-
-	<p class="text-xl">{product?.name ?? 'Name'}</p>
-	<p class="text-lg my-2">{product?.price ?? 'Price'}</p>
-	<p class="text-base my-2">{product?.description ?? 'Description'}</p>
-
+<div>
 	<div
-		class="fixed bottom-0 left-0 w-full place-items-center bg-gray-800 text-black p-4 flex flex-row-reverse"
+		class="fixed top-0 left-0 w-full bg-gray-800 text-black p-4 flex justify-between z-10"
 	>
+		<Button
+			variant="outline"
+			class="center px-2"
+			on:click={() => {
+				if (browser) window.history.back();
+			}}
+		>
+			<ArrowBigLeft class="h-6 w-6" color="#000000" />
+		</Button>
+
 		<Button
 			variant="outline"
 			class="center px-2 self-end"
 			on:click={() => {
-				addToCart();
+				goto(`/cart`)
 			}}
 		>
 			<ShoppingCart class="h-6 w-6" color="#000000" />
-			<p class="px-2">Add to Cart</p>
 		</Button>
 	</div>
 
-	<div class="border mt-4 mb-2" />
+	<div class="mx-auto px-6 py-20">
+		<OfferCarousels offerPagerItem={bannerImages} onOfferClicked={() => {}} />
 
-	<p class="text-lg my-4">Similar Products</p>
+		<p class="text-xl">{product?.name ?? 'Name'}</p>
+		<p class="text-lg my-2">{product?.price ?? 'Price'}</p>
+		<p class="text-base my-2">{product?.description ?? 'Description'}</p>
 
-	<div class="grid grid-cols-2 gap-4">
-		{#each similarProducts as product}
-			<button
-				class="border-2 border-sky-500 rounded-lg w-36 h-52 text-wrap"
-				on:click={(e) => {
-					goto(`/category/${product.categoryId}/product/${product.productId}`);
+		<div
+			class="fixed bottom-0 left-0 w-full place-items-center bg-gray-800 text-black p-4 grid grid-cols-2 grid-rows-1"
+		>
+			<Button
+				variant="outline"
+				class="center px-2 self-end"
+				on:click={() => {
+					goto(`/cart`);
 				}}
 			>
-				<div
-					class="bg-cover bg-center w-full h-3/4 overflow-clip"
-					style={`background-image: url(${product.imageUrls[0]})`}
-				/>
-				<div class="truncate text-center">{product.name}</div>
-				<div class="text-md text-center">{product.price}</div>
-			</button>
-		{/each}
-	</div>
+				<ShoppingBag class="h-6 w-6" color="#000000" />
+				<p class="px-2">Checkout</p>
+			</Button>
 
-	<div class="py-10"/>
+			<Button
+				variant="outline"
+				class="center px-2 self-end"
+				on:click={() => {
+					addToCart();
+				}}
+			>
+				<ShoppingCart class="h-6 w-6" color="#000000" />
+				<p class="px-2">Add to Cart</p>
+			</Button>
+		</div>
+
+		<div class="border mt-4 mb-2" />
+
+		<p class="text-lg my-4">Similar Products</p>
+
+		<div class="grid grid-cols-2 gap-4">
+			{#each similarProducts as product}
+				<button
+					class="border-2 border-sky-500 rounded-lg w-36 h-52 text-wrap"
+					on:click={(e) => {
+						console.log(product);
+						console.log(`/category/${product.categoryId}/product/${product.productId}`);
+						goto(`/category/${product.categoryId}/product/${product.productId}`);
+					}}
+				>
+					<div
+						class="bg-cover bg-center w-full h-3/4 overflow-clip"
+						style={`background-image: url(${product.imageUrls[0]})`}
+					/>
+					<div class="truncate text-center">{product.name}</div>
+					<div class="text-md text-center">{product.price}</div>
+				</button>
+			{/each}
+		</div>
+
+		<div class="py-10" />
+	</div>
 </div>
